7bb30bce2c00873b0d7327e7f9ab48a974ccf739
Luis Pérez <luisp@yelp.com>
2020-12-07 22:34:10 -0800

    fix volume and @/^

--- keyboards/moonlander/keymaps/nemacysts/keymap.c
+++ keyboards/moonlander/keymaps/nemacysts/keymap.c
@@ -33,6 +33,7 @@ enum custom_keycodes {
     R_BRACKET_6,
     BANG_8,
     POUND_TICK,
+    AT_CARET
 };
 
 enum layers {
@@ -43,10 +44,10 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_moonlander(
         TILDE_DOLLAR, AMPERSAND_PERCENT, L_BRACKET_7, L_BRACE_5, R_BRACE_3, L_PAREN_1, EQL_9, /* split */ STAR_0, R_PAREN_2, PLUS_4, R_BRACKET_6, BANG_8, POUND_TICK, KC_BSPACE,
-        KC_TAB, KC_SCLN, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, /* split */ KC_G, KC_C, KC_R, KC_L, KC_SLSH, KC_AT, KC_ENT,
+        KC_TAB, KC_SCLN, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, /* split */ KC_G, KC_C, KC_R, KC_L, KC_SLSH, AT_CARET, KC_ENT,
         KC_LCTRL, KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, /* split */ KC_H, KC_T, KC_N, KC_S, KC_MINUS, KC_BSLS, KC_ENT,
         KC_LSFT, KC_QUOTE, KC_Q, KC_J, KC_K, KC_X, /* split */ KC_B, KC_M, KC_W, KC_V, KC_Z, KC_LSFT,
-        KC_LCTRL, KC_LOPT, KC_LCMD, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, /*thumb*/ KC_HYPR,/* split */ KC_MEH, /* thumb*/ KC__VOLUP, KC__VOLDOWN, KC_LCMD, KC_LOPT, RGB_TOG,
+        KC_LCTRL, KC_LOPT, KC_LCMD, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, /*thumb*/ KC_HYPR,/* split */ KC_MEH, /* thumb*/ KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN, KC_LCMD, KC_LOPT, RGB_TOG,
                                             KC_SPC,  KC_BSPC, KC_LGUI, /* split */ KC_LALT,  KC_TAB,  KC_ENT
     ),
 };
@@ -172,6 +173,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     set_mods(current_mods);
                 } else {
                     SEND_STRING("#");
+                }
+                return false;
+            case AT_CARET:
+                if (shifted) {
+                    clear_mods();
+                    SEND_STRING("^");
+                    set_mods(current_mods);
+                } else {
+                    SEND_STRING("@");
                 }
                 return false;
         }
